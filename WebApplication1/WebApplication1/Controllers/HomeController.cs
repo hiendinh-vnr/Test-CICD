d200298cbf8ecd873ca1cdfe6824af7977ec7801
hiendinh-vnr <hien.dinh@vnresource.org>
2022-11-19 11:41:39 +0700

    Có lỗi khi build Solution

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -8,9 +8,7 @@ namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
+        
         }
 
         public ActionResult About()
